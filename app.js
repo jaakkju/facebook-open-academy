9229e61f8870122240515adbbaf79b0d278a9cb9
Juhani Jaakkola <juhani@jaakkola.net>
2013-12-03 20:52:23 +0100

    added error handling

--- app.js
+++ app.js
@@ -6,30 +6,28 @@
  *
  * Call chaing in this solution after creating http.createServer if file does not exist
  * getZipZile -> unZipData -> analyzeData -> printAnalyzation
+ *
+ * json.zip is unzipped to filesJSON then analyzed and result stored analyzationResult
+ * This result is passed to browser when requested
+ *
+ * Set print true in config.json if you want analyzation result to be printed to console
  */
 
- 
- /**
- * Module dependencies.
- */
-
+// Module dependencies
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var AdmZip = require('adm-zip');
+var admZip = require('adm-zip');
 
 var app = express();
 
-// file variables
-var file_name = 'json.zip';
-var file_url = 'http://pilvilinna.cert.fi/opendata/autoreporter/json.zip';
-var file_analyzation = __dirname + '/cert-fi_data/analyzation.json';
-var file_dest = __dirname + '/cert-fi_data/' + file_name;
-var files_JSON;
+// Configuration file
+var config = require(path.join(__dirname, 'config.js'));
 
-
-var analyzation = {
+// Variables for storing JSON files and analyzation data
+var filesJSON;
+var analyzationResult = {
 	done : false,
 	from : undefined,
 	to : undefined,
@@ -57,92 +55,91 @@ if ('development' == app.get('env')) {
 
 app.get('/', function(req, res) {
 	res.render('index', {
-		title : 'Facebook’s Open Academy Program - Juhani Jaakkola',
-		analyzation : analyzation
+		title : 'CERT-FI: Open Data - Statistics from Autoreporter',
+		analyzationResult : analyzationResult
 	});
 });
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 
-	function printAnalyzation() {
-		console.log('Incident categories and occurrencies frequency/day between ' + analyzation.from + ' - ' + analyzation.to);
-		console.log(analyzation.from);
-		console.log(analyzation.to);
-		for (var key in analyzation.categories) {
-			console.log(key + ' ' + analyzation.categories[key] / analyzation.days);
+	function printAnalyzationResult() {
+		console.info('Incident categories and occurrencies frequency/day between ' + analyzationResult.from + ' - ' + analyzationResult.to + '\n');
+
+		for (var key in analyzationResult.categories) {
+			console.log(key + ' ' + analyzationResult.categories[key] / analyzationResult.days);
 		}
 
-		console.log('Incident locations and occurrencies between ' + analyzation.from + ' - ' + analyzation.to);
-		for (var key in analyzation.locations) {
-			console.log(key + ' ' + analyzation.locations[key]);
+		console.info('Incident locations and occurrencies between ' + analyzationResult.from + ' - ' + analyzationResult.to + '\n');
+
+		for (var key in analyzationResult.locations) {
+			console.log(key + ' ' + analyzationResult.locations[key]);
 		}
 	}
 
-	function analyzeData() {
-		console.log("Data structure ready.. analyzing data...");
+	function analyzeJSON() {
+		console.info("Data structure ready, starting to analyze data...");
 
-		var file_counter = files_JSON.length;
-		files_JSON.forEach(function(JSON_file) {
-			file_counter--;
+		var fileCounter = filesJSON.length;
+		filesJSON.forEach(function(fileJSON) {
+			fileCounter--;
 
-			var day_counter = JSON_file.autoreporter.opendata.length;
-			JSON_file.autoreporter.opendata.forEach(function(day) {
-				day_counter--;
+			var dayCounter = fileJSON.autoreporter.opendata.length;
+			fileJSON.autoreporter.opendata.forEach(function(day) {
+				dayCounter--;
 
 				// Capturing start and end dates
 				var dateFrom = new Date(day.date.from);
-				if (analyzation.from == undefined || analyzation.from.getTime() > dateFrom.getTime()) {
-					analyzation.from = dateFrom;
-				}
+				if (analyzationResult.from == undefined || analyzationResult.from.getTime() > dateFrom.getTime())
+					analyzationResult.from = dateFrom;
 
 				var dateTo = new Date(day.date.to);
-				if (analyzation.to == undefined || analyzation.to.getTime() < dateTo.getTime()) {
-					analyzation.to = dateTo;
-				}
+				if (analyzationResult.to == undefined || analyzationResult.to.getTime() < dateTo.getTime())
+					analyzationResult.to = dateTo;
 
-				var occurrency_counter = day.asn.length;
+				var occurrencyCounter = day.asn.length;
 				day.asn.forEach(function(occurrency) {
-					occurrency_counter--;
+					occurrencyCounter--;
 
-					var ipaddress_counter = occurrency.ipaddress.length;
+					var ipaddressCounter = occurrency.ipaddress.length;
 					occurrency.ipaddress.forEach(function(ipaddress) {
-						ipaddress_counter--;
+						ipaddressCounter--;
 
 						// Creating new location to object if does not exist
 						// TODO there is probably a nicer way to write this
-						if (analyzation.locations.hasOwnProperty(ipaddress.city)) {
-							analyzation.locations[ipaddress.city]++;
+						if (analyzationResult.locations.hasOwnProperty(ipaddress.city)) {
+							analyzationResult.locations[ipaddress.city]++;
 						} else {
-							analyzation.locations[ipaddress.city] = 1;
+							analyzationResult.locations[ipaddress.city] = 1;
 						}
 
-						var incident_counter = ipaddress.incident.length;
+						var incidentCounter = ipaddress.incident.length;
 						ipaddress.incident.forEach(function(incident) {
-							incident_counter--;
+							incidentCounter--;
 
 							// Creating new category to object if does not exist
 							// TODO there is probably a nicer way to write this
-							if (analyzation.categories.hasOwnProperty(incident.category.main)) {
-								analyzation.categories[incident.category.main]++;
+							if (analyzationResult.categories.hasOwnProperty(incident.category.main)) {
+								analyzationResult.categories[incident.category.main]++;
 							} else {
-								analyzation.categories[incident.category.main] = 1;
+								analyzationResult.categories[incident.category.main] = 1;
 							}
 
 							// After every loop is done, print the result and setting analyzation done
-							if (file_counter == 0 && day_counter == 0 && occurrency_counter == 0 && ipaddress_counter == 0 && incident_counter == 0) {
+							if (fileCounter == 0 && dayCounter == 0 && occurrencyCounter == 0 && ipaddressCounter == 0 && incidentCounter == 0) {
 								var oneDay = (1000 * 60 * 60 * 24);
-								analyzation.days = Math.abs((analyzation.from.getTime() - analyzation.to.getTime()) / (oneDay));
+								analyzationResult.days = Math.abs((analyzationResult.from.getTime() - analyzationResult.to.getTime()) / (oneDay));
 
-								analyzation.done = true;
+								analyzationResult.done = true;
 
-								fs.writeFile(file_analyzation, JSON.stringify(analyzation), function(err) {
+								fs.writeFile(config.pathAnalyzation, JSON.stringify(analyzationResult), function(err) {
 									if (err)
 										throw err;
-									console.log('Analyzation saved to file ' + file_analyzation);
+									console.log('Analyzation saved to file ' + config.pathAnalyzation);
 								});
 
-								printAnalyzation();
+								if (config.print)
+									printAnalyzationResult();
 							}
 						});
 					});
@@ -152,11 +149,11 @@ http.createServer(app).listen(app.get('port'), function() {
 	};
 
 	function unZipData() {
-		var zip = new AdmZip(file_dest);
+		var zip = new admZip(config.pathJSON);
 		var zipEntries = zip.getEntries();
 
-		console.log(file_name + " contains " + zipEntries.length + " files, extracting to array");
-		files_JSON = new Array(zipEntries.length);
+		console.info(config.JSON + " contains " + zipEntries.length + " files, extracting to array");
+		filesJSON = new Array(zipEntries.length);
 
 		// Unzippping everything to data array as JSON objects then exec callback
 		var index = 0;
@@ -165,23 +162,25 @@ http.createServer(app).listen(app.get('port'), function() {
 
 			zip.readAsTextAsync(entry, function(data) {
 				console.log('Decompressing ' + entryName + ' as a JSON object to array index ' + index);
-				files_JSON[index++] = JSON.parse(data);
+				filesJSON[index++] = JSON.parse(data);
 
-				if (index == zipEntries.length) {
-					analyzeData();
-				}
+				if (index == zipEntries.length)
+					analyzeJSON();
 			});
 		});
 	};
 
 	function getZipfile() {
-		console.log('Downloading file.. ' + file_url);
+		if (!path.existsSync(config.pathFolder))
+			fs.mkdirSync(config.pathFolder);
 
 		var lenght = 0;
-		var file = fs.createWriteStream(file_dest);
-		var request = http.get(file_url, function(response) {
+		var file = fs.createWriteStream(config.pathJSON);
+		var request = http.get(config.JSON_url, function(response) {
 
 			var change = -1;
+			console.log('Downloading file.. ' + config.JSON_url);
+
 			response.on('data', function(chunk) {
 				file.write(chunk);
 				lenght += chunk.length;
@@ -190,44 +189,50 @@ http.createServer(app).listen(app.get('port'), function() {
 
 				if (change != percent) {
 					change = percent;
-					console.log('Downloading file ' + file_name + " " + percent + '%');
+					process.stdout.write(" " + percent + "%");
 				}
 			});
 
 			response.on('end', function() {
+				console.log();
 				file.close();
-				console.log(file_name + ' file downloaded');
 			});
 
 			file.on('close', function() {
-				unZipData(analyzeData);
+				unZipData();
 			});
 		});
 	};
 
-	// TODO testing
-	// TODO error handling, http, unzip, analyzation
+	// Cathing errors
+	try {
+		fs.exists(config.pathAnalyzation, function(exists) {
+			// If analyzation exists we just read the data to variable
+			if (exists) {
+				fs.readFile(config.pathAnalyzation, function(err, data) {
+					if (err)
+						throw err;
 
-	fs.exists(file_analyzation, function(exists) {
-		// If analyzation exists we just read the data to variable
-		if (exists) {
-			fs.readFile(file_analyzation, function(err, data) {
-				if (err)
-					throw err;
+					console.log('Analyzation file exist: ' + config.pathAnalyzation);
+					analyzationResult = JSON.parse(data.toString());
+					if (config.print)
+						printAnalyzationResult();
+				});
+			} else {
+				// Analyzation file does not exist, checking json.zip
+				fs.exists(config.pathJSON, function(exists) {
+					if (exists) {
+						unZipData();
+					} else {
+						// json.zip does not exist downloading file
+						console.warn(config.JSON + ' file does not exist...');
+						getZipfile();
+					}
+				});
+			}
+		});
+	} catch (err) {
+		console.error("Error: " + err);
+	}
 
-				analyzation = JSON.parse(data.toString());
-			});
-		} else {
-
-			// Analyzation file does not exist, checking json.zip
-			fs.exists(file_dest, function(exists) {
-				if (exists) {
-					unZipData(analyzeData);
-				} else {
-					// json.zip does not exist downloading file
-					getZipfile();
-				}
-			});
-		}
-	});
 });
